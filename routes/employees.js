4c10a81e249777dd95e01d7b4e9807e1e15e4c29
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-27 09:37:06 +0300

    Routes for employee crud

---
+++ routes/employees.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+
+const EmployeeCtrl = require('../controllers/employee')
+
+const router = express.Router()
+
+router.post('/', EmployeeCtrl.createEmployee)
+router.put('/:id', EmployeeCtrl.updateEmployee)
+router.delete('/:id', EmployeeCtrl.deleteEmployee)
+router.get('/:id', EmployeeCtrl.getEmployeeById)
+router.get('/', EmployeeCtrl.getEmployees)
+
+module.exports = router
