8b4b43e79ec6487b47d18fefb3ad8721b3a38267
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-27 09:35:30 +0300

    Initialize express app from scratch

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = require('./db')
+const employeeRouter = require('./routes/employees')
+
+const app = express()
+const apiPort = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', function(){
+    // we are connected to Database
+    console.log('Connected to MongoDB');
+})
+
+app.use('/employees', employeeRouter)
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
